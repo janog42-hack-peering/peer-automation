a6c693c68e9382eafb4395c78a054ea2f780fcec
kojiyama <kojiyama@k-opti.com>
2018-07-11 15:12:05 +0900

    First Commit

---
+++ test.py
@@ -0,0 +1,15 @@
+from bottle import route, run
+from socket import gethostname, gethostbyname
+
+
+
+
+def bgpsummary():
+    return("result")
+
+
+@route('/bgp')
+def bgp():
+    return("TEST")
+
+run(host='localhost', port=8080, debug=True)
