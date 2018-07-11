3633e806ca4777c77b5b1513b9bd37f408cd10f4
Yuya Kusakabe <y-kusakabe@sakura.ad.jp>
2018-07-11 17:26:05 +0900

    pdb: add ix_id option to get_peer_info

--- pdb/__init__.py
+++ pdb/__init__.py
@@ -3,11 +3,13 @@ import requests
 base_url = 'https://www.peeringdb.com/api/'
 
 
-def get_peer_info(asn=0):
-    if asn == 0:
+def get_peer_info(asn=0, ix_id=0):
+    if asn == 0 or ix_id == 0:
         return None
     params = {'asn': asn}
     r = requests.get(base_url+'net', params)
     id = r.json()['data'][0]['id']
     r = requests.get(base_url+'net/'+str(id))
-    return r.json()
+    for ix in r.json()['data'][0]['netixlan_set']:
+        if ix['ix_id'] == ix_id:
+            return ix
